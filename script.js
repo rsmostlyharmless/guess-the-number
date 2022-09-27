e24bb6c8a5bfae5982ff597a6d246cb030d356f1
Ryan.Syder <rsmostlyharmless@gmail.com>
2022-09-27 18:26:41 +0200

    removed cluttering comments

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 'use strict';
-// console.log(document.querySelector(`.message`).textContent);
-// document.querySelector(`.message`).textContent = `🎉 Correct Number! 🎉`;
-// document.querySelector(`.number`).textContent = 13;
-// document.querySelector(`.score`).textContent = 10;
-// document.querySelector(`.guess`).value = 13;
-// console.log(document.querySelector(`.guess`).value);
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 5;
@@ -21,11 +15,9 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
     // no guess
     if (!guess) {
         display(`🙈 Input a number! 🙈`);
-        // document.querySelector(`.message`).textContent = `🙈 Input a number! 🙈`;
 
         // correct guess
     } else if (guess == secretNumber) {
-        // document.querySelector(`.message`).textContent = `🎉 Correct Number! 🎉`;
         display(`🎉 Correct Number! 🎉`);
         document.querySelector(`.number`).textContent = secretNumber;
         document.querySelector(`body`).style.backgroundColor = `#60b347`;
@@ -44,8 +36,6 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
         // guess is wrong
     } else if (guess != secretNumber) {
         if (score > 1) {
-            //   document.querySelector(`.message`).textContent =
-            //     guess > secretNumber ? `👆 Too High! 👆` : `👇 Too Low! 👇`;
             display(guess > secretNumber ? `👆 Too High! 👆` : `👇 Too Low! 👇`);
             score--;
             document.querySelector(`.score`).textContent = score;
@@ -56,15 +46,13 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
             document.querySelector(`.score`).textContent = 0;
             document.querySelector(`body`).style.backgroundColor = `#d31414`;
             document.querySelector(`.number`).textContent = `☠️`;
-            //   document.querySelector(`.message`).textContent = `☠️ You Lose! ☠️`;
         }
     }
 });
-
+// reset game
 document.querySelector(`.again`).addEventListener(`click`, function () {
     score = 5;
     secretNumber = Math.trunc(Math.random() * 20) + 1;
-    //   document.querySelector(`.message`).textContent = `Start guessing...`;
     display(`Start guessing...`);
     document.querySelector(`.score`).textContent = score;
     document.querySelector(`.number`).textContent = `?`;
@@ -72,34 +60,3 @@ document.querySelector(`.again`).addEventListener(`click`, function () {
     document.querySelector(`.guess`).value = '';
     document.querySelector(`body`).style.backgroundColor = `#222`;
 });
-
-// guess too high
-//   } else if (guess > secretNumber) {
-//     if (score > 1) {
-//       document.querySelector(`.message`).textContent = `👆 Too High! 👆`;
-//       score--;
-//       document.querySelector(`.score`).textContent = score;
-//     } else {
-//       scoreCounter = scoreCounter - scoreCounter;
-//       document.querySelector(`.score-counter`).textContent = scoreCounter;
-//       document.querySelector(`.message`).textContent = `☠️ You Lose! ☠️`;
-//       document.querySelector(`.score`).textContent = 0;
-//       document.querySelector(`body`).style.backgroundColor = `#d31414`;
-//       document.querySelector(`.number`).textContent = `☠️`;
-//     }
-
-//     // guess too low
-//   } else if (guess < secretNumber) {
-//     if (score > 1) {
-//       document.querySelector(`.message`).textContent = `👇 Too Low! 👇`;
-//       score--;
-//       document.querySelector(`.score`).textContent = score;
-//     } else {
-//       scoreCounter = scoreCounter - scoreCounter;
-//       document.querySelector(`.score-counter`).textContent = scoreCounter;
-//       document.querySelector(`.message`).textContent = `☠️ You Lose! ☠️`;
-//       document.querySelector(`.score`).textContent = 0;
-//       document.querySelector(`body`).style.backgroundColor = `#d31414`;
-//       document.querySelector(`.number`).textContent = `☠️`;
-//     }
-//   }
